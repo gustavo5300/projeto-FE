676b1a4adb4e53a0a326d4826358145f2a3243a8
gustavo5300 <antunesggustavo@gmail.com>
2025-06-18 15:26:03 -0300

    Add files via upload

---
+++ LandPage/digianscript.js
@@ -0,0 +1,42 @@
+function irparaSobre() {
+    const imagem = document.getElementById('isobre');
+    const posicaoY = imagem.getBoundingClientRect().top;
+   
+    window.scrollTo( {
+        top: posicaoY,
+        behavior: 'smooth'
+    });
+
+}
+function irparaServ() {
+    const imagem = document.getElementById('iserv');
+    const posicaoY = imagem.getBoundingClientRect().top;
+    
+    window.scrollTo( {
+        top: posicaoY,
+        behavior: 'smooth'
+    });
+    
+}
+function irparaContato() {
+    const imagem = document.getElementById('icontato');
+    const posicaoY = imagem.getBoundingClientRect().top;
+   
+    window.scrollTo( {
+        top: posicaoY,
+        behavior: 'smooth'
+    });
+    
+
+}
+function irparainicio() {
+    const imagem = document.getElementById('inicio');
+    const posicaoY = imagem.getBoundingClientRect().top;
+    
+    window.scrollTo( {
+        top: posicaoY,
+        behavior: 'smooth'
+    });
+
+
+}
